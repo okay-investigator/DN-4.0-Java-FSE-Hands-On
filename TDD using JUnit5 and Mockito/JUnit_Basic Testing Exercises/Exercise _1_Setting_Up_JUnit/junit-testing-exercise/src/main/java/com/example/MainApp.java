643d367e0f6a10aac7aef08466c0e7d6db2b1a32
okay-investigator <131176975+okay-investigator@users.noreply.github.com>
2025-07-20 15:04:13 +0530

    Delete TDD using JUnit5 and Mockito directory

--- TDD using JUnit5 and Mockito/JUnit_Basic Testing Exercises/Exercise _1_Setting_Up_JUnit/junit-testing-exercise/src/main/java/com/example/MainApp.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class MainApp {
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-}
--- TDD using JUnit5 and Mockito/JUnit_Basic Testing Exercises/Exercise_3_Assertions_in_JUnit/src/main/java/com/example/MainApp.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class MainApp {
-    public static int add(int a, int b) {
-        return a + b;
-    }
-}
